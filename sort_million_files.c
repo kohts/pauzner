0ab2fa66034ef2f6498e8d536b273b6ece300912
Petya Kohts <petya@kohts.com>
2014-01-16 21:23:14 +0000

    sort_million_files: small step forward * ftm.c * generate million files: lstat + strerror_r()

--- sort_million_files.c
+++ sort_million_files.c
@@ -1,16 +1,61 @@
 #include <min_heap.h>
 
+// lstat
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// errno
+#include <errno.h>
+
 boolean generate_million_files (char *dest_dir) {
+    struct stat stat_buf;
+
+    int res = lstat(dest_dir, &stat_buf);
+    if (res != 0) {
+        int orig_errno = errno;
+        char error_message[STRUCT_NAME_LENGTH];
+
+        if (strerror_r(errno, error_message, (size_t) STRUCT_NAME_LENGTH) == 0) {
+            die("lstat [%s] returned [%d]: %s", dest_dir, res, error_message);
+        }
+        else {
+            die("lstat [%s] returned [%d]: errno %d, additionally strerror_r failed with errno %d", dest_dir, res, orig_errno, errno);
+        }
+    }
+
+    if (S_ISDIR(stat_buf.st_mode)) {
+        printf("%s is dir\n", dest_dir);
+    }
+    
     return true;
 }
 
-int main(int argc, char *argv[]) {
+void usage()
+{
+    die("%s\n%s\n%s\n\n",
+        "  usage: ",
+        "    sort_million_files --generate <DESTINATION_PATH> <NUMBER_OF_FILES>",
+        "    sort_million_files --sort <SOURCE_PATH> --dest <FILE>"
+        );
+}
+
+int main(int argc, char *argv[])
+{
     char dest_dir[STRUCT_NAME_LENGTH];
 
-    if (argc == 3) {
+    if (argc == 5) {
+        if (strcmp("--sort", argv[1]) == 0 && strcmp("--dest", argv[3]) == 0) {
+            printf("here\n");
+        }
+        else {
+            usage();
+        }
+    }
+    else if (argc == 4) {
         if (strcmp("--generate", argv[1]) == 0) {
             if (strlen(argv[2]) < 1) {
-                die("usage: sort_million_files --generate <DESTINATION_PATH> %d 123", 10);
+                die("usage example: sort_million_files --generate <DESTINATION_PATH> 10");
             }
             else if (strlen(argv[2]) > STRUCT_NAME_LENGTH) {
                 die("<DESTINATION_DIR> must be no longer than %d", STRUCT_NAME_LENGTH);
@@ -26,12 +71,13 @@ int main(int argc, char *argv[]) {
             }
         }
         else {
-            die("usage: sort_million_files --generate <DESTINATION_PATH>");
+            usage();
         }
 
         return 0;
     }
-    else if (argc == 2) {
+    else {
+        usage();
     }
 
     return 0;
